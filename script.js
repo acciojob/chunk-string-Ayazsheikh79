8b606a53c25bdc080afed1be1d6f31b72de688d2
Mohammad Ayaz <91006780+Ayazsheikh79@users.noreply.github.com>
2026-03-03 13:20:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function stringChop(str, size) {
   // your code here
-	return (str.match(/.{1,5}/g))
+	if (!str) return []
+	const regex = new RegExp(`.{1, ${size}}`, "g")
+	return str.match(regex) || []
 }
 
 // Do not change the code below
